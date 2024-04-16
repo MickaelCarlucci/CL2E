f060aa7f09d0f8d0cd2c1e91723735b470267d43
MickaelCarlucci <frewmike17@gmail.com>
2024-04-16 22:41:08 +0200

    add css

--- app/index.app.js
+++ app/index.app.js
@@ -13,6 +13,7 @@ const app = express();
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('app/integration'));
 app.use(router);
 
 export default app;
