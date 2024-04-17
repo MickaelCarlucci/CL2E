53608672bbc0b5c32a541e2af9abad09da538203
MickaelCarlucci <frewmike17@gmail.com>
2024-04-17 22:06:44 +0200

    finish homePage, and add controller for Form

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,33 @@
+import sgMail from "@sendgrid/mail";
+
 const mainController = {
 homePage: (request, response) => {
 response.render('home.ejs')
+},
+
+contact: (request, response) => {
+    response.render('contact.ejs')
+},
+
+sendEmail: (request, response) => {
+    const { name, email, message } = request.body;
+    const mailOptions = {
+        to: 'frewmike17@gmail.com',
+        from: email,
+        subject: `Vous avez reçu un nouveau message de ${name}`,
+        text: `${message}\n\n--\n${name}\n${email}`,
+    };
+      // Envoi de l'e-mail via SendGrid
+  sgMail.send(mailOptions)
+  .then(() => {
+    console.log('E-mail envoyé avec succès');
+    res.status(200).send('E-mail envoyé avec succès');
+  })
+  .catch((error) => {
+    console.error('Erreur lors de l\'envoi de l\'e-mail :', error.response.body);
+    res.status(500).send('Erreur lors de l\'envoi de l\'e-mail');
+  });
+
 }
 }
 
--- app/routers/router.js
+++ app/routers/router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 
 router.get('/', mainController.homePage);
+router.get('/contact', mainController.contact);
 
 export default router;
